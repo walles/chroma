7764c349cd0a0e32ea9fe558855716bcf7f8dda7
Johan Walles <johan.walles@gmail.com>
2024-01-13 08:22:14 +0100

    Make startup 4ms faster
    This change takes this metric down from 21ms to 17ms on my machine:
    
    ```
    cd cmd/chroma
    go build && ./chroma < /dev/null && time ./chroma < /dev/null
    ```

--- styles/api.go
+++ styles/api.go
@@ -4,6 +4,7 @@ import (
 	"embed"
 	"io/fs"
 	"sort"
+	"sync"
 
 	"github.com/alecthomas/chroma/v2"
 )
@@ -19,21 +20,38 @@ var Registry = func() map[string]*chroma.Style {
 	if err != nil {
 		panic(err)
 	}
+
+	mutex := sync.Mutex{}
+	var wg sync.WaitGroup
+
 	for _, file := range files {
 		if file.IsDir() {
 			continue
 		}
-		r, err := embedded.Open(file.Name())
-		if err != nil {
-			panic(err)
-		}
-		style, err := chroma.NewXMLStyle(r)
-		if err != nil {
-			panic(err)
-		}
-		registry[style.Name] = style
-		_ = r.Close()
+
+		wg.Add(1)
+
+		// FIXME: What happens if this goroutine panics?
+		go func(file fs.DirEntry) {
+			defer wg.Done()
+
+			r, err := embedded.Open(file.Name())
+			if err != nil {
+				panic(err)
+			}
+			style, err := chroma.NewXMLStyle(r)
+			if err != nil {
+				panic(err)
+			}
+			_ = r.Close()
+
+			mutex.Lock()
+			defer mutex.Unlock()
+			registry[style.Name] = style
+		}(file)
 	}
+
+	wg.Wait()
 	return registry
 }()
 
